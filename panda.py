2dbda8b32738f3eabefc60266a8e1f828ca483be
ashwin-kwatra <ashwin.kwatra.5@gmail.com>
2026-07-18 14:58:19 +0530

    adding dataframes

--- panda.py
+++ panda.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import sys 
 np.set_printoptions(suppress=True)
 import time 
 #making basic series with arrays
@@ -47,9 +48,33 @@ print(musclee.isnull().sum())
 # musclee.rename(columns={"sleep_hours":"daily_sleep"}) ------- renames the column
 # this change is temprory , to make it permament , use INPLACE
 # sum , min , max , median etc functions work
+ 
+ # to fetch data from dataframes :
+
 print(type(musclee[['sleep_hours','weight_kg']])) # multiple columns = dataframe
 print(type(musclee["day"])) # single columns= series
+# musclee.set_index("day") ->>>>>>>>>>>>> turns the day column into the index column isntead of default
+print(musclee.iloc[0])  # this also gives a series as an answer 
+print(musclee.iloc[[0,1,2]]) # this gives dataframe
+print(musclee.iloc[0:3,0:3]) # first 2 rows and first 3 columns only
+print(musclee.loc[0:2,['day','weight_kg']]) # isme column ka name dena pdta h 
+restday=musclee["workout_volume_kg"]==0
+print(musclee[restday]) # filtering data
+musclee["carbs"]=0          #creates a new column with defualt value =0
+print(musclee.head(3))
 
-
-
-
+# this function is sued to get a series 
+# if we dont assign column and dont use squeze , it gets a dataframe
+vk = pd.read_csv('kohli_ipl.csv',index_col='match_no').squeeze()
+print(vk.head())
+print(type(vk))
+print(sys.getsizeof(vk))
+print(sys.getsizeof(vk.astype('Int16'))) # reducing size
+print(vk.info)
+print(vk[vk.between(51,99)])
+temp=pd.Series([1,2,np.nan,1,2,3,np.nan,4,np.nan,5,6,1,2,3,7,8,np.nan,np.nan])
+#inplace=True laga skte h
+# nan values ka duplicate bhi count karta hai
+print(temp.drop_duplicates())
+print(temp.duplicated().sum())
+print(temp.isnull())
